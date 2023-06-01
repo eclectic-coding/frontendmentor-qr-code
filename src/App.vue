4db374d740a5a9a70c09dc8fe762eac8dc87c136
Chuck Smith <csmit223@nd.edu>
2023-06-01 16:16:59 -0400

    Finish up

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,7 @@
 <script setup>
+import TheCard from "@/components/TheCard.vue";
 </script>
 
 <template>
-  <h1>hello</h1>
+  <TheCard/>
 </template>
-
-<style scoped>
-</style>
---
+++ src/components/TheCard.vue
@@ -0,0 +1,42 @@
+<script setup></script>
+
+<template>
+  <div class="card">
+    <img src="../assets/images/image-qr-code.png" alt="" />
+    <div class="card__body">
+      <h2>Improve your front-end skills by building projects</h2>
+      <div>
+        Scan the QR code to visit Frontend Mentor and take your coding skills to the next level
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.card {
+  max-width: 20rem;
+  height: 31rem;
+  background-color: var(--color-white);
+  box-shadow: 0 1.25rem 1.25rem rgba(0, 0, 0, 0.0476518);
+  border-radius: 1.25rem;
+
+  img {
+    width: 100%;
+    border-radius: 10px;
+    padding: 1rem;
+  }
+
+  &__body {
+    padding: 0 1rem 2.5rem 1rem;
+
+    h2 {
+      color: var(--text-header);
+      font-size: 1.375rem;
+      font-style: normal;
+      font-weight: 700;
+      line-height: 28px;
+      margin-bottom: 1rem;
+    }
+  }
+}
+</style>
